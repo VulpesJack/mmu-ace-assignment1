741e6c19dd6b0f3854a75c7f1b1174599dfc61ca
Jack Noble <foxpaw.jack@gmail.com>
2018-02-20 13:22:26 +0000

    Create pirParallaxTest.cpp

---
+++ pirParallaxTest.cpp
@@ -0,0 +1,31 @@
+#include "mbed.h"
+
+Serial pc(USBTX, USBRX);
+//DigitalOut led(LED1);
+DigitalIn pirIn(P11);
+
+int val = 0;
+int cnt = 0;
+
+int main() {
+  pc.printf("Init NRF\n");
+  
+  wait(5);
+  pc.printf("Snapshot\n");
+  
+  while(1) {
+    val = pirPin.read();
+    
+    if (val == 0) {
+      cnt++;
+      
+      pc.printf("Sensor ON, Detected %i \r\n",cnt);
+      pc.printf("Stay Away!");
+      
+      wait(1.5f);
+    }
+    else {
+      pc.printf("Sensor is OFF");
+    }
+  }
+}
