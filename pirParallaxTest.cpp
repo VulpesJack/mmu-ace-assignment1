7d107f266542866b1ec7a73cc2322a637e48e5a6
Jack Noble <foxpaw.jack@gmail.com>
2018-02-20 13:48:42 +0000

    Update pirParallaxTest.cpp

--- pirParallaxTest.cpp
+++ pirParallaxTest.cpp
@@ -4,8 +4,6 @@ Serial pc(USBTX, USBRX);
 //DigitalOut led(LED1);
 DigitalIn pirPin(p11, PullUp);
 
-int val = 0;
-
 int main() {
   pc.printf("Init NRF\n");
   
@@ -13,7 +11,7 @@ int main() {
   pc.printf("Snapshot\n");
   
   while(1) {
-    val = pirPin.read();
+    int val = pirPin.read();
     
     pc.printf("val = %f\n",val);
   }
