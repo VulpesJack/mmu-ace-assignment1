7b41c29fa431f4b6f3f156823986bceb0366074c
Jack Noble <foxpaw.jack@gmail.com>
2018-02-21 10:19:21 +0000

    Update dhtMbedTest.cpp
    Added serial lines

--- dhtMbedTest.cpp
+++ dhtMbedTest.cpp
@@ -3,21 +3,23 @@
 
 DigitalOut dhtLed(LED);
 
+Serial pc(USBTX, USBRX);
+
 DHT sensor(p23, DHT11);
 
 int main() {
 	int err;
-	printf("INIT DHT");
+	pc.printf("INIT DHT");
 	wait(1);
 	while(1) {
 		dhtLED = 1;
 		err = sensor.readData();
 		if (err == 0) {
-			printf("Temp = %4.2f C\r\n",sensor.ReadTemperature(CELCIUS));
-			printf("Humidity = %4.2f \r\n",sensor.ReadHumidity());
+			pc.printf("Temp = %4.2f C\r\n",sensor.ReadTemperature(CELCIUS));
+			pc.printf("Humidity = %4.2f \r\n",sensor.ReadHumidity());
 		}
 	} else
-		printf("\r\nErr %i \n",err);
+		pc.printf("\r\nErr %i \n",err);
 	dhtLed = 0;
 	wait(5);
 }
