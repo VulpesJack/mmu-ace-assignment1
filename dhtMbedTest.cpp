49e3da22a8c809b4a9499c10ee7b2f0c2e9d4bc0
Jack Noble <foxpaw.jack@gmail.com>
2018-02-23 12:54:43 +0000

    Update dhtMbedTest.cpp

--- dhtMbedTest.cpp
+++ dhtMbedTest.cpp
@@ -18,8 +18,9 @@ int main() {
 			pc.printf("Temp = %4.2f C\r\n",sensor.ReadTemperature(CELCIUS));
 			pc.printf("Humidity = %4.2f \r\n",sensor.ReadHumidity());
 		} else {
-		pc.printf("\r\nErr %i \n",err);
+			pc.printf("\r\nErr %i \n",err);
+		}
+		dhtLed = 0;
+		wait(5);
 	}
-	dhtLed = 0;
-	wait(5);
 }
