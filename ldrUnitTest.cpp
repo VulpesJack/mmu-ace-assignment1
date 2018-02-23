8a992727b1f173b534d01b2bd4124018fbdf44ec
Jack Noble <foxpaw.jack@gmail.com>
2018-02-23 14:09:41 +0000

    Update ldrUnitTest.cpp

--- ldrUnitTest.cpp
+++ ldrUnitTest.cpp
@@ -8,15 +8,23 @@ int main() {
 	pc.printf("INIT\n");
 	
 	float Vo;
+	float R = 10000;
+	float Vin = 3.3;
+	
 	pc.printf("LDR GO\n");
 	
 	while(1){
 		Vo = ldr.read();
 		
 		float pd = 1.7833/Vo;
+		float Rt = (Vo*R)/(Vin - Vo);
+		float FTC = (powf(10,5.57644))*(powf(Rt,-1.211743));
+		float Lux = FTC*10.76;
 		
 		printf("Sensor Reading (Vo): %f V\r\n", Vo);
 		printf("Sensor Reading (PD): %f W/m^2\r\n\r\n", pd);
+		printf("FTC: %f\r\n",FTC);
+		printf("Lux: %f\r\n",Lux);
 		
 		wait(2);
 	}
